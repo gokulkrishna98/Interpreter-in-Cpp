3388644eeea7acaa1c1b895fa2ccd0ad5024f068
gokulkrishna98 <gokulkrishna98@gmail.com>
2025-01-21 23:05:56 -0500

    [repl] modified reply to print AST string.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,23 @@
 // #include "lexer.h"
 #include "parser.h"
-// #include "repl.h"
+#include "repl.h"
 
 using namespace std;
 
 int main(int argc, char** argv){
-    // printf("interp running\n");
+    printf("interp running\n");
     // lexer::test_next_token();
-    // repl::start();
-    parser::test_let_statements();
-    parser::test_ret_statements();
+    repl::start();
+    // parser::test_let_statements();
+    // parser::test_ret_statements();
     // parser::test_string();
     // parser::test_identifier_expression();
     // parser::test_integer_literal_expression();
     // parser::test_parsing_prefix_expression();
     // parser::test_parsing_infix_expression();
-    parser::test_operator_precedence_parsing();
+    // parser::test_operator_precedence_parsing();
     // parser::test_if_expression();
-    parser::test_call_expression();
+    // parser::test_call_expression();
     
     return 0;
 }
--- repl.cpp
+++ repl.cpp
@@ -1,24 +1,48 @@
 #include "repl.h"
-#include "lexer.h"
+#include "parser.h"
 #include <string>
 #include <iostream>
 
 using namespace std;
 
 namespace repl {
+
+const char* MONKEY_FACE = R"(
+               ',
+            .-`-,\__
+              ."`   `,
+            .'_.  ._  `;.
+        __ / `      `  `.\ .--.
+       /--,| 0)   0)     )`_.-,)
+      |    ;.-----.__ _-');   /
+       '--./         `.`/  `"`
+          :   '`      |.
+          | \     /  //
+           \ '---'  /'
+            `------' \
+             _/       `--...
+                  __
+)";
 const string PROMPT = ">>>"; 
 void start(){
     string input;
     while(true){
         cout << PROMPT;
-        cin.ignore();
         getline(cin, input);
         lexer::Lexer l(input);
-        lexer::Token tok = l.next_token();
-        while(tok.type != lexer::TokenType::ENDOF){
-            printf("%s\n", tok.val.c_str());
-            tok = l.next_token();
+        auto p = parser::Parser(l);
+        auto program = p.parse_program();
+        if(!p.errors.empty()){
+            std::cout << MONKEY_FACE << std::endl;
+            std::cerr << "parser errors:" << std::endl;
+            for(auto s: p.errors){
+                cout << "\t" << s << std::endl;
+            }
+            continue;
         }
+        std::cout << std::endl;
+        std::cout << program->string() << std::endl;
+
     }
 }
 } // namespace repl
